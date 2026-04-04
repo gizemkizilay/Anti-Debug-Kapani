283aa2a838ab56a1f243fd9625417bbef1db975a
gizem <kizilaygzm@gmail.com>
2026-04-04 23:53:27 +0300

    Vize projesi kaynak kodları ve çalışan exe dosyası eklendi

---
+++ _deps/capstone-build/CMakeFiles/capstone.dir/arch/SystemZ/SystemZMCTargetDesc.c.obj.d
@@ -0,0 +1,17 @@
+_deps/capstone-build/CMakeFiles/capstone.dir/arch/SystemZ/SystemZMCTargetDesc.c.obj: \
+ C:\Users\gizem\.gemini\antigravity\scratch\AntiDebugTrap\build\_deps\capstone-src\arch\SystemZ\SystemZMCTargetDesc.c \
+ C:/Users/gizem/.gemini/antigravity/scratch/AntiDebugTrap/build/_deps/capstone-src/include/capstone/platform.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdbool.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/x86_64-w64-mingw32/include/inttypes.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdint.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/x86_64-w64-mingw32/include/stdint.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ C:/Users/gizem/AppData/Local/Microsoft/WinGet/Packages/BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe/mingw64/x86_64-w64-mingw32/include/stddef.h \
+ C:\Users\gizem\.gemini\antigravity\scratch\AntiDebugTrap\build\_deps\capstone-src\arch\SystemZ\SystemZMCTargetDesc.h \
+ C:\Users\gizem\.gemini\antigravity\scratch\AntiDebugTrap\build\_deps\capstone-src\arch\SystemZ\SystemZGenRegisterInfo.inc
